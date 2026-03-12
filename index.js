772a59817ca26ab6600fb7a93405119037aa0d9d
Thomas <boy1.thoomas@gmail.com>
2026-03-12 14:06:00 +0100

    feat: add mock pokemon data and add route to get pokemon

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
+const pokemons = require("./mock-pokemon");
 
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Express");
+  res.send("Application web Pokémon");
+});
+
+app.get("/api/pokemon", (req, res) => {
+  res.send(pokemons);
+});
+
+app.get("/api/pokemon/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+  res.send(`Vous avez demandé le Pokémon ${pokemon.name}`);
 });
 
 app.listen(PORT, () => {
---
+++ mock-pokemon.js
@@ -0,0 +1,28 @@
+let pokemons = [
+  {
+    id: 1,
+    name: "Bulbizarre",
+    hp: 45,
+    cp: 49,
+    types: ["Plante", "Poison"],
+    created: new Date(),
+  },
+  {
+    id: 2,
+    name: "Salamèche",
+    hp: 39,
+    cp: 52,
+    types: ["Feu"],
+    created: new Date(),
+  },
+  {
+    id: 3,
+    name: "Carapuce",
+    hp: 44,
+    cp: 48,
+    types: ["Eau"],
+    created: new Date(),
+  },
+];
+
+module.exports = pokemons;
