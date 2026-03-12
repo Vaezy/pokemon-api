c33782466dd9954f89dd8677d97c3ec4920a9d89
Thomas <boy1.thoomas@gmail.com>
2026-03-12 14:32:51 +0100

    feat(model): add User Sequelize model

---
+++ src/models/user.js
@@ -0,0 +1,10 @@
+export default (sequelize, DataTypes) => {
+  return sequelize.define("User", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    username: {
+      type: DataTypes.STRING,
+      unique: { msg: "Ce nom d'utilisateur est déjà pris." },
+    },
+    password: { type: DataTypes.STRING },
+  });
+};
